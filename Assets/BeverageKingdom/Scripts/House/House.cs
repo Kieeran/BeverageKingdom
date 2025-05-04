c157990390c5e0b2d2b50b227c2f7b697b4399f5
Quốc Bảo <21521856@gm.uit.edu.vn>
2025-05-04 15:20:18 +0700

    Healthbar for houses

--- Assets/BeverageKingdom/Scripts/House/House.cs
+++ Assets/BeverageKingdom/Scripts/House/House.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class House : MonoBehaviour
 {
+    public Image HealthBarFillUI;
+
+    [HideInInspector]
     public float HP;
+
+    public float MaxHP;
+
+    void Start()
+    {
+        MaxHP = HP;
+    }
 
     public void DecreaseHouseHP()
     {
         if (HP == 0) return;
 
         HP--;
+        HealthBarFillUI.fillAmount = HP / MaxHP;
         if (HP == 0)
         {
             Invoke("DelayAndGameOver", 1f);
