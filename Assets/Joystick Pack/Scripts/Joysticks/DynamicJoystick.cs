359d934b9e6910ddfed97a1418930cd138745d78
Quốc Bảo <21521856@gm.uit.edu.vn>
2025-05-11 11:02:45 +0700

    Changes

--- Assets/Joystick Pack/Scripts/Joysticks/DynamicJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/DynamicJoystick.cs
@@ -7,26 +7,31 @@ public class DynamicJoystick : Joystick
 {
     public float MoveThreshold { get { return moveThreshold; } set { moveThreshold = Mathf.Abs(value); } }
 
+    Vector3 originPos;
+
     [SerializeField] private float moveThreshold = 1;
 
     protected override void Start()
     {
         MoveThreshold = moveThreshold;
         base.Start();
-        background.gameObject.SetActive(false);
+        // background.gameObject.SetActive(false);
+        originPos = background.anchoredPosition3D;
     }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
         background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
-        background.gameObject.SetActive(true);
+        // background.gameObject.SetActive(true);
         base.OnPointerDown(eventData);
     }
 
     public override void OnPointerUp(PointerEventData eventData)
     {
-        background.gameObject.SetActive(false);
+        // background.gameObject.SetActive(false);
         base.OnPointerUp(eventData);
+
+        background.anchoredPosition3D = originPos;
     }
 
     protected override void HandleInput(float magnitude, Vector2 normalised, Vector2 radius, Camera cam)
