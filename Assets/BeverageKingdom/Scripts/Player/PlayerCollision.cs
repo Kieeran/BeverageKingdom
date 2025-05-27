148874649071ae035494ab8b1ea647942e79b3d6
Quốc Bảo <21521856@gm.uit.edu.vn>
2025-05-27 23:34:24 +0700

    Player collect item

--- Assets/BeverageKingdom/Scripts/Player/PlayerCollision.cs
+++ Assets/BeverageKingdom/Scripts/Player/PlayerCollision.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        if (collision.CompareTag("Item"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
