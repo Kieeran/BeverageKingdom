20c4f0313bc9cf0206693535d419677c5e4eb369
HoangAnh <vobahoanganh2005@gmail.com>
2025-05-29 14:42:27 +0700

    fix loi part 1

--- Assets/BeverageKingdom/Scripts/ItemDrop/ItemSpawner.cs
+++ Assets/BeverageKingdom/Scripts/ItemDrop/ItemSpawner.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class ItemSpawner : Spawner
 {
     public static ItemSpawner Instance;
-    private int spawnRate = 10; // spawn rate in seconds
+    private int spawnRate = 50; // spawn rate in seconds
 
     protected override void Awake()
     {
         Instance = this;
+        spawnRate = 90;
     }
 
     public void Spawn(Vector3 position, Quaternion rotation)
@@ -15,6 +16,7 @@ public class ItemSpawner : Spawner
         Transform item = RandomPrefabs();
         // them ti le spawn item
         int a = Random.Range(0, 100);
+        Debug.Log("Spawn Rate: " + a + " / " + spawnRate);    
         //GameObject item = Instantiate(itemPrefab, position, rotation, parent);
         if (a < spawnRate)
         {
