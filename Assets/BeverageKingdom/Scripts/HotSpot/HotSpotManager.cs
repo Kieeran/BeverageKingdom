4c50fc0fb0571ab837110ead1593d2d95c2cdba2
Quốc Bảo <21521856@gm.uit.edu.vn>
2025-06-17 14:43:28 +0700

    Changes

--- Assets/BeverageKingdom/Scripts/HotSpot/HotSpotManager.cs
+++ Assets/BeverageKingdom/Scripts/HotSpot/HotSpotManager.cs
@@ -18,7 +18,7 @@ public class HotSpotManager : MonoBehaviour
 
     private void Start()
     {
-        // TrySpawnHotSpots();
+        TrySpawnHotSpots();
     }
 
     private IEnumerator SpawnRoutine()
